1fd4a8392774768f7c3b25e235c9932607e38578
Thomas <namulabre@gmail.com>
2024-04-27 02:24:09 +0200

    Added the possibility to give in argument the directory to upload

--- main.go
+++ main.go
@@ -3,13 +3,42 @@ package main
 import (
 	"autoUpload/minio"
 	"autoUpload/utils"
+	"errors"
 	"fmt"
 	_ "github.com/joho/godotenv/autoload"
 	"os"
 )
 
+func parseArgs(args []string) (string, error) {
+	filePath := args[0]
+	info, err := os.Stat(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	if info.IsDir() {
+		return filePath, nil
+	}
+	return "", errors.New("path must be a directory")
+}
+
+func getRootDir() string {
+	argsWithoutProgram := os.Args[1:]
+
+	if len(argsWithoutProgram) != 0 {
+		rootDir, err := parseArgs(argsWithoutProgram)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			panic("Cannot get information on upload directory.\n Try to add \"/\" at the end of the directory name")
+		}
+		return rootDir
+	} else {
+		return os.Getenv("ROOT_DIR_PATH")
+	}
+}
+
 func main() {
-	rootDir := os.Getenv("ROOT_DIR_PATH")
+	rootDir := getRootDir()
 
 	filesData, err := utils.GetAllFilesPaths(rootDir)
 	if err != nil {
